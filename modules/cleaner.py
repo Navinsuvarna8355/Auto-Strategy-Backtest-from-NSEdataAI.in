b34fc4bc92fbe2fdf32d2ca80af158300e9789f2
Navinsuvarna8355 <156652548+Navinsuvarna8355@users.noreply.github.com>
2025-08-25 20:28:15 +0530

    Update cleaner.py

--- modules/cleaner.py
+++ modules/cleaner.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import zipfile
+import os
+
+def extract_and_clean(zip_path):
+    try:
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall("data/bhavcopy/")
+        csv_file = zip_path.replace(".zip", "")
+        df = pd.read_csv(csv_file)
+        df = df[df['SERIES'] == 'EQ']
+        df = df[['SYMBOL', 'OPEN', 'HIGH', 'LOW', 'CLOSE', 'TOTTRDQTY', 'TIMESTAMP']]
+        df.columns = ['symbol', 'open', 'high', 'low', 'close', 'volume', 'date']
+        df['date'] = pd.to_datetime(df['date'], format='%d-%b-%Y')
+        return df
+    except Exception as e:
+        print(f"Clean error: {e}")
+        return pd.DataFrame()
 
