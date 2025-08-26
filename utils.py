6bfc1cc8e06a3ace4db0af3da4c9fa96dd3de8ed
Navinsuvarna8355 <156652548+Navinsuvarna8355@users.noreply.github.com>
2025-08-26 12:26:39 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import pytz
+import streamlit as st
+
+def convert_to_IST(df):
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df['timestamp'] = df['timestamp'].dt.tz_localize('UTC').dt.tz_convert('Asia/Kolkata')
+    return df
+
+def export_csv(df, filename):
+    csv = df.to_csv(index=False).encode('utf-8')
+    st.download_button("📥 Download Trade Log", data=csv, file_name=filename, mime='text/csv')
 
